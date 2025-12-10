9a797dcb2bce1e34e3442e8f518e1ac9871d5ccc
xumpocmb <youarenear26@gmail.com>
2025-12-10 11:37:17 +0300

    chore(config): add static and media file settings for debug and production

--- _settings/settings.py
+++ _settings/settings.py
@@ -131,6 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # DRF Configuration
